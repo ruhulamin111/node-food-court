673e40d9df8d94042a31e8c156d1c42b1cc3f614
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-06 12:15:15 +0600

    data load

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -15,4 +16,24 @@ app.listen(port, () => {
     console.log('working port', port);
 })
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect()
+        const foodCollection = client.db('food').collection('item')
+
+        app.get('/food', async (req, res) => {
+            const query = {}
+            const cursor = foodCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
