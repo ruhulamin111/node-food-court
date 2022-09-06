dc77a575ebc54c2a41ab3fccd685702a5a79ab87
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-06 22:08:32 +0600

    delete method done

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/food/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await foodCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
     }
     finally {
