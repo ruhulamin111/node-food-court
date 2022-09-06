becb021b747f8ad09fc887440bec8d936a685c22
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-06 11:19:48 +0600

    node food court start

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('food court server')
+})
+
+app.listen(port, () => {
+    console.log('working port', port);
+})
+
+
