1be160ddd74d85939e46dae0f71d44e70f032b67
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-06 20:05:23 +0600

    data update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -12,9 +12,6 @@ app.get('/', (req, res) => {
     res.send('food court server')
 })
 
-app.listen(port, () => {
-    console.log('working port', port);
-})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -24,10 +21,17 @@ async function run() {
         await client.connect()
         const foodCollection = client.db('food').collection('item')
 
-        app.get('/food', async (req, res) => {
-            const query = {}
-            const cursor = foodCollection.find(query)
-            const result = await cursor.toArray()
+        app.get('/foods', async (req, res) => {
+            const query = {};
+            const cursor = foodCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/food/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await foodCollection.findOne(query)
             res.send(result)
         })
 
@@ -37,3 +41,8 @@ async function run() {
     }
 }
 run().catch(console.dir)
+
+
+app.listen(port, () => {
+    console.log('working port', port);
+})
