4ec8b7e43781ae8dd08c57e4278690799a7033d3
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-08 16:27:15 +0600

    site done

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unothorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
+
 app.get('/', (req, res) => {
     res.send('food court server')
 })
@@ -17,8 +34,11 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run() {
+
     try {
+
         await client.connect()
         const foodCollection = client.db('food').collection('item')
         const orderCollection = client.db('food').collection('order')
@@ -29,24 +49,24 @@ async function run() {
             res.send({ accessToken })
         })
 
-        app.get('/order', async (req, res) => {
-            const authHeader = req.headers.authorization;
-            console.log(authHeader);
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const search = req.query.email;
-            const query = { email: search }
-            const cursor = orderCollection.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
+            if (decodedEmail === search) {
+                const query = { email: search }
+                const cursor = orderCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            } else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
         })
-
 
         app.post('/order', async (req, res) => {
             const query = req.body;
             const result = await orderCollection.insertOne(query)
             res.send(result)
         })
-
-
 
         app.get('/foods', async (req, res) => {
             const query = {};
@@ -75,14 +95,12 @@ async function run() {
             res.send(result)
         })
 
-
     }
     finally {
 
     }
 }
 run().catch(console.dir)
-
 
 app.listen(port, () => {
     console.log('working port', port);
